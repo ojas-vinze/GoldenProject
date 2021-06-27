cca3c8a26b5971e0792e92bfa7b44bd2462d7190
ojas-vinze <71540421+ojas-vinze@users.noreply.github.com>
2021-06-27 10:12:03 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -39,22 +39,32 @@ class Game{
         var x=100;
         var y=200;
         var index=0;
-        drawSprites();
+
+        this.spawnBalloons();
 
         for(var plr in allPlayers){
         
             index = index+1;
-            y=500-allPlayers[plr].distance;
+            y=500+allPlayers[plr].distance;
             x=500;
             
-            players[index-1].x = x;
-            players[index-1].y = y;
+            // players[index-1].x = x;
+            // players[index-1].y = y;
 
             if(index===player.index){
                 fill("black");
                 textSize(20);
                 text(player.name,x,y);
+
+                stroke("red");
+                ellipse(players[index-1].x,players[index-1].y,50,50);
             }
+        }
+
+        if(frameCount%50===0){
+            var rand=random(20,570);
+            balloon=createSprite(10,rand,20,50);
+               
         }
 
         if(keyDown(UP_ARROW)){
@@ -73,6 +83,38 @@ class Game{
             arrow.velocityX=-4;
             arrow.lifetime=350;
             arrowg.add(arrow);
+        }
+        drawSprites();
+    }
+
+    spawnBalloons(){
+        
+        var posx=100;
+        var posy=100;
+        for(var i=0; i<40; i++){
+            if(i%10===0){
+                posy=posy+50;
+                posx=100;
+            }
+            balloon=createSprite(posx,posy,50,50);
+            posx=posx+50;
+
+            // var rande=Math.round(random(1,4));
+            switch(posy){
+                case 100: balloon.addImage(balloon1img);
+                balloon.scale=0.08;
+                    break;
+                case 150: balloon.addImage(balloon2img);
+                balloon.scale=0.08;
+                    break;
+                case 200: balloon.addImage(balloon3img);
+                    break;
+                case 250: balloon.addImage(balloon4img);
+                balloon.scale=0.08;
+                    break;
+                default:break;
+            }
+            balloong.add(balloon);
         }
     }
 
