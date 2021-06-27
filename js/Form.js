abd368e58cf8a34b9469f6fb3c1d0c3117ea27a8
ojas-vinze <71540421+ojas-vinze@users.noreply.github.com>
2021-06-27 10:12:01 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -10,32 +10,33 @@ class Form{
     display(){
         this.title.html("Welcome to the ballon buster game!!!");
         this.title.position(350,50);
+        this.title.style("color","blue");
 
         this.input.position(width/2,height/2);
 
         this.button.position(width/2+40,height/2+50);
-        this.button.mousePressed(fnToPlay())
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide()
+
+            playerCount++;
+            player.updateCount(playerCount);
+            player.name=this.input.value();
+            player.update();
+
+            if(playerCount<2){
+                this.greeting.html("Hello " + player.name + ", you have to wait for the 2nd player to join");
+            }
+        })
 
         this.reset.position(550,20);
 
-        this.greeting.position(displayWidth/2,displayHeight/2);
+        this.greeting.position(250,190);
 
         this.reset.mousePressed(()=>{
             player.updateCount(0);
             game.update(0);
         })
-    }
-
-    fnToPlay(){
-        clear()
-        playerCount++;
-        player.updateCount(playerCount);
-        player.name=this.input.value();
-        player.update();
-        player.index=playerCount;
-        if(playerCount<2){
-        this.greeting.html("Hello"+player.name);
-        }
     }
 
     hide(){
