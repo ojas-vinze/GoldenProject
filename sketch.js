3abd33bdfa9b156e8b8aac3212c108eef3ea8e3e
ojas-vinze <71540421+ojas-vinze@users.noreply.github.com>
2021-08-29 11:01:23 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,17 +4,19 @@ var gameState=0;
 var arrowimg,bowimg,backimg,balloon1img,balloon2img,balloon3img,balloon4img;
 var player1,player2;
 var players;
-var bg1img,bg2img;
+var bg1img,bg2img,bg3img;
 var arrow,balloon;
 var arrowg,balloong;
 var game,player,form;
 var database;
+var createBalloon = true;
 
 
 function preload(){
     bowimg=loadImage("Images/bow.png");
     bg1img=loadImage("Images/Welcome2.png");
     bg2img=loadImage("Images/bg.png");
+    bg3img=loadImage("Images/gameoverscreen.png");
     arrowimg=loadImage("Images/arrow.png");
     balloon1img=loadImage("Images/blueballoon.png");
     balloon2img=loadImage("Images/greenballoon.png");
@@ -35,5 +37,10 @@ function setup(){
 function draw(){
     if(playerCount===2){
         game.play();
+    }
+
+    if(gameState===2){
+        game.update(2);
+       game.leaderboard();
     }
 }
